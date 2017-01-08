d3f09ca1566d20a285190f1ef9886c36e4cf25c5
daniel <dcarmo@alumni.uoguelph.ca>
2017-01-08 11:24:31 -0500

    Updated FaceItem creation to the factory pattern

---
+++ Demo/FaceItemFactory.swift
@@ -0,0 +1,56 @@
+//
+//  FaceItemFactory.swift
+//  Demo
+//
+//  Created by Daniel Carmo on 2017-01-08.
+//  Copyright © 2017 ModiFace Inc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FaceItemFactory {
+    
+    static func build(forFacePosition position:FacePosition, imageName:String) -> FaceItem {
+        switch position {
+        case .head:
+            return FaceItem(position: .head,
+                            anchorPosition: .above,
+                            anchorPoint: CGPoint(x:0.5, y:1),
+                            imageName: imageName,
+                            centerOffset: CGPoint(x:0, y:-75),
+                            widthMultiplier: 2.0)
+        case .eyes:
+            return FaceItem(position: .eyes,
+                            anchorPoint: CGPoint(x:0.5, y:1),
+                            imageName: imageName,
+                            widthAdjustment:20.0)
+        case .leftEye:
+            return FaceItem(position: .leftEye,
+                            anchorPoint: CGPoint(x:0.5, y:1),
+                            imageName: imageName,
+                            widthAdjustment:20.0)
+        case .rightEye:
+            return FaceItem(position: .rightEye,
+                            anchorPoint: CGPoint(x:0.5, y:1),
+                            imageName: imageName,
+                            widthAdjustment:20.0)
+        case .nose:
+            return FaceItem(position: .nose,
+                            anchorPoint: CGPoint(x:0.5, y:1),
+                            imageName: imageName,
+                            widthAdjustment:20.0)
+        case .upperLip:
+            return FaceItem(position: .upperLip,
+                            anchorPoint: CGPoint(x:0.5, y:1),
+                            imageName: imageName,
+                            widthAdjustment:60.0)
+        case .centerMouth:
+            return FaceItem(position: .centerMouth,
+                            anchorPoint: CGPoint(x:0.5, y:1),
+                            imageName: imageName,
+                            widthAdjustment:20.0)
+        }
+    }
+    
+}
--- Demo/FaceItemsProvider.swift
+++ Demo/FaceItemsProvider.swift
@@ -12,58 +12,15 @@ import UIKit
 class FaceItemProvider {
     
     static let items = [
-        FaceItem(position: .head,
-                 anchorPosition: .above,
-                 anchorPoint: CGPoint(x:0.5, y:1),
-                 imageName: "hat",
-                 centerOffset: CGPoint(x:0, y:-75),
-                 widthMultiplier: 2.0),
-        
-        FaceItem(position: .head,
-                 anchorPosition: .above,
-                 anchorPoint: CGPoint(x:0.5, y:1),
-                 imageName: "horns",
-                 centerOffset: CGPoint(x:0, y:-75),
-                 widthMultiplier: 2.0),
-        
-        FaceItem(position: .head,
-                 anchorPosition: .above,
-                 anchorPoint: CGPoint(x:0.5, y:1),
-                 imageName: "light",
-                 centerOffset: CGPoint(x:0, y:-75),
-                 widthMultiplier: 2.0),
-        
-        FaceItem(position: .eyes,
-                 anchorPoint: CGPoint(x:0.5, y:1),
-                 imageName: "heart",
-                 widthAdjustment:20.0),
-        
-        FaceItem(position: .nose,
-                 anchorPoint: CGPoint(x:0.5, y:1),
-                 imageName: "dog_nose",
-                 widthAdjustment:20.0),
-        
-        FaceItem(position: .nose,
-                 anchorPoint: CGPoint(x:0.5, y:1),
-                 imageName: "pig_nose",
-                 widthAdjustment:20.0),
-        
-        FaceItem(position: .centerMouth,
-                 anchorPoint: CGPoint(x:0.5, y:1),
-                 imageName: "lips",
-                 widthAdjustment:20.0),
-        
-        FaceItem(position: .centerMouth,
-                 anchorPoint: CGPoint(x:0.5, y:1),
-                 imageName: "dog_tongue",
-                 centerOffset: CGPoint(x: 0, y: 40)),
-        
-        FaceItem(position: .upperLip,
-                 anchorPoint: CGPoint(x:0.5, y:1),
-                 imageName: "mustache",
-                 //centerOffset: CGPoint(x:0, y:-10),
-                 widthAdjustment:60.0)
-        
+        FaceItemFactory.build(forFacePosition: .head, imageName: "hat"),
+        FaceItemFactory.build(forFacePosition: .head, imageName: "horns"),
+        FaceItemFactory.build(forFacePosition: .head, imageName: "light"),
+        FaceItemFactory.build(forFacePosition: .eyes, imageName: "heart"),
+        FaceItemFactory.build(forFacePosition: .nose, imageName: "dog_nose"),
+        FaceItemFactory.build(forFacePosition: .nose, imageName: "pig_nose"),
+        FaceItemFactory.build(forFacePosition: .centerMouth, imageName: "lips"),
+        FaceItemFactory.build(forFacePosition: .centerMouth, imageName: "dog_tongue"),
+        FaceItemFactory.build(forFacePosition: .upperLip, imageName: "mustache")        
     ]
     
     
